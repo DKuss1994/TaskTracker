727c2ef08fadaa98416e5bd30e7faedfa2b283a1
DKuss1994 <kussdennis@web.de>
2025-12-08 21:55:23 +0100

    add UML

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main {
             String hello = "hello";
             ArrayList<String> x = new ArrayList<>();
             x.add("hello");
-            
+
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
